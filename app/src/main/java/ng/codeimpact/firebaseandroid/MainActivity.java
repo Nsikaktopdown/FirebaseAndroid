5a87a5ebd1d12bdadac1d0a990f5d3cd0540a527
NsikakApp <nsikakthompson73@gmail.com>
2017-02-15 21:31:52 +0100

    Added: StaggeredGridLayout to the Recyclerview

--- app/src/main/java/ng/codeimpact/firebaseandroid/MainActivity.java
+++ app/src/main/java/ng/codeimpact/firebaseandroid/MainActivity.java
@@ -4,19 +4,52 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.List;
+
+import ng.codeimpact.firebaseandroid.models.List_item;
+import ng.codeimpact.firebaseandroid.viewholder.ListViewHolder;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "ListActivity";
+
+    // [define_database_reference]
+    private DatabaseReference mDatabase;
+
+    private FirebaseRecyclerAdapter<List_item, ListViewHolder> mAdapter;
+    private RecyclerView mRecycler;
+    private LinearLayoutManager mManager;
+    FirebaseListAdapter<List> ad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        // [create_database_reference]
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        
+        mRecycler = (RecyclerView) findViewById(R.id.messages_list);
+        int columnCount = getResources().getInteger(R.integer.list_column_count);
+        StaggeredGridLayoutManager sglm =
+                new StaggeredGridLayoutManager(columnCount, StaggeredGridLayoutManager.VERTICAL);
+        mRecycler.setLayoutManager(sglm);
+        mRecycler.setHasFixedSize(true);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +59,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
     }
 
     @Override
